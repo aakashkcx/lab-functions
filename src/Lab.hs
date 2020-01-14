c530b8edd7e993da2770a5c511f785a1a7dd0bc6
Aakash Kc <aakashkcx@gmail.com>
2020-01-14 17:51:51 +0000

    complete

--- src/Lab.hs
+++ src/Lab.hs
@@ -16,40 +16,47 @@ import Prelude hiding (not, and, max)
 
 -- 0. Define `age` to be your age.
 
-age = 28
+age = 18
 
 -- 1. Define `name` to be your name.
 
-name = "Michael"
+name = "Aakash"
 
 -- 2. Modify the definition of `triple` so that it triples its argument.
 
-triple = \x -> x * 2
+triple = \x -> x * 3
 
 -- 3. Replace `undefined` in the definition of `tripleV2` below so that
 -- it behaves like `triple` above, but uses the syntactic sugar for
 -- lambda terms instead.
 
-tripleV2 x = undefined
+tripleV2 x = x * 3
 
 -- 4. Replace `undefined` in the definition of `not` below to implement
 -- boolean negation.
 
-not = undefined
+-- not = \x -> if x == True then False else True
+-- not x = if x == True then False else True
+not True = False
+not False = True
 
 -- 5. Replace `undefined` in the definition of `and` below to implement
 -- boolean conjunction.
 
-and = undefined
+-- and = \x -> \y -> if x == True && y == True then True else False
+and True True = True
+and x y = False
 
 -- 6. Replace `undefined` in the definition of `max` below so that it reduces
 -- to the greater number of `x` and `y`.
 
-max x y = undefined
+-- max x y = if x > y then x else y
+max x y | x > y = x
+        | otherwise = y
 
 -- 7. Replace `undefined` in the definition of `perimeterRect` so that it
 -- calculates the perimeter of a rectangle given its length and width.
 
-perimeterRect l w = undefined
+perimeterRect l w = 2 * (l + w)
 
 --------------------------------------------------------------------------------
